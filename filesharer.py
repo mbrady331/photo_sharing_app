38c6c76584e4b778e074bc31f291559ec54ddf10
mbrady331 <mbrady331@gmail.com>
2021-06-23 14:50:01 -0500

    Update filesharer.py

--- filesharer.py
+++ filesharer.py
@@ -2,7 +2,7 @@ from filestack import Client
 
 class FileShare:
 
-    def __init__(self, filepath, api_key = 'A2UWvpWIRii8epuTYcPwLz'):
+    def __init__(self, filepath, api_key = 'your api key here'):
         self.filepath = filepath
         self.api_key = api_key
 
